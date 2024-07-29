d96eb489c4f9d9ea7f4587b25e683454ef38075b
Akpan Nnamnso <nnamnso.akpan@student.aul.edu.ng>
2024-07-29 06:56:54 +0100

    Menu added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var mainContent = document.getElementById('main-content');
+    sidebar.classList.toggle('collapsed');
+    mainContent.classList.toggle('collapsed');
+}
